a7b573b155405bcf7556a662b6456b54409581fc
huangli <huanglixianhui@163.com>
2020-09-01 16:58:32 +0800

    完成算法

--- PatternMatchingOfStrings/PatternMatchingOfStrings/stringmatching.h
+++ PatternMatchingOfStrings/PatternMatchingOfStrings/stringmatching.h
@@ -3,6 +3,40 @@
 #define	STRINGMATCHING_H_
 #include <iostream>
 
+const int MAXSIZE = 1000;
+typedef struct
+{
+	char* ch;
+	int length;
+}Sstring;
+
+void creatString(Sstring &str)
+{
+	str.ch = new char[MAXSIZE];
+	str.length = 0;
+}
+
+int Index_BF(Sstring S, Sstring T, int pos)
+{
+	int i = pos, j = 1;
+	while (i <= S.length && j <= T.length)
+	{
+		if (S.ch[i] == T.ch[j])
+		{
+			++i;
+			++j;
+		}
+		else
+		{
+			i = i - j + 2;
+			j = 1;
+		}
+	}
+	if (j > T.length)
+		return i - T.length;
+	else
+		return 0;
+}
 //BF string match algo
 
 //KMP string match algo
